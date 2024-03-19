56f958d398ab5842ba6ca5c514dadd3415ca26a2
MageswariP23 <157380517+MageswariP23@users.noreply.github.com>
2024-03-18 20:36:36 -0700

    Add files via upload

---
+++ design.js
@@ -0,0 +1,8 @@
+function bulbon(){
+    document.getElementById("img").src="https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg" ;
+
+}
+function bulboff()
+{
+    document.getElementById("img"). src="https://i.postimg.cc/KjK1wL3c/bulb-off.png" ;
+}
